8fd1d4613501b6931e6f515854b95537cc5cb799
Soumyanil Podder <soumyanilpodder@Soumyanils-MacBook-Air-2.local>
2026-05-28 18:57:49 +0530

    Add TestNG test for Selenium automation

--- src/test/java/galaxyautomation/GalaxyTest.java
+++ src/test/java/galaxyautomation/GalaxyTest.java
@@ -9,12 +9,14 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class GalaxyTest {
 
-	public static void main(String[] args) throws InterruptedException {
+	@Test
+	public void openWebsiteTest() {
 
 		WebDriverManager.chromedriver().setup();
 
